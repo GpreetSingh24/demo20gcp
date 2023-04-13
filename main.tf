201f5744b3e804561f35f6d23dba54ea050bee95
GpreetSingh24 <130556990+GpreetSingh24@users.noreply.github.com>
2023-04-13 14:45:33 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  credentials = file("terraform-key.json")
+  project     = "acn-deliverylearning-devops"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+# Create a GCS Bucket
+
+resource "google_storage_bucket" "mybucket" {
+  name     = "vishnubucket-<date>"
+  location = "us-central1"
+}
+
